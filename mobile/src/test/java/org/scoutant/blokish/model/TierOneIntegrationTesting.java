c4fba6afab556cd7d1f6c02c3b3c5f0560cfb0c6
PatrickGhazal <patrick.ghazal@mail.mcgill.ca>
2018-11-11 16:34:45 -0500

    Create TierOneIntegrationTesting.java
    JUnit Test Case for Tier One

---
+++ mobile/src/test/java/org/scoutant/blokish/model/TierOneIntegrationTesting.java
@@ -0,0 +1,39 @@
+package org.scoutant.blokish.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TierOneIntegrationTesting {
+	
+	Game game;
+	TierOneStubsDrivers elements;
+	
+	@Before
+	public void setUp() {
+		game = new Game();
+	}	
+	
+	@Test
+	public void unitTestGame() {
+		// Asserting that the game was created properly, i.e. has 4 boards, no moves, and the game is not over.
+		assertEquals(game.boards.size(), 4);
+		assertEquals(game.moves.size(), 0);
+		assertFalse(game.over());
+		
+		// Testing one arbitrary method of the Game class
+		game.historize(new Move(new Piece(), 1, 2));
+		assertEquals(game.moves.size(), 1);
+		assertEquals(game.moves.get(0).j, 2);
+	}
+	
+	@Test
+	public void integTestGameAI() {
+		// Asserting that the hasMove method of the AI class returns the expected result of false.
+		elements = new TierOneStubsDrivers(game);
+		assertFalse(elements.hasMove(0));
+	}
+
+}
