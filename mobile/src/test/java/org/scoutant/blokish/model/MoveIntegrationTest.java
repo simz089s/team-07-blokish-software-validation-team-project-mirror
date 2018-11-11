de826f21a3dc210e2e2a1a546f45887d91ca3ed3
Elvric <elvric.trombert@mail.mcgill.ca>
2018-11-11 15:36:13 -0500

    Tier 2 commit

---
+++ mobile/src/test/java/org/scoutant/blokish/model/MoveIntegrationTest.java
@@ -0,0 +1,33 @@
+package org.scoutant.blokish.model;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class MoveIntegrationTest {
+
+    Move move;
+
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception{
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        Piece piece_mock = new Piece(3, "L4", 4, 2).add(0,-1).add(0,0).add(0,1).add(1,1);
+        move = new Move(piece_mock, 1, 1);
+    }
+
+    // integration testing of Move with Piece (composition)
+    // test: serialize(Move move) method
+    @Test
+    public void testMovePieceCompositionOnserializeMethodInMovex() {
+        String serializedStr = "";
+        serializedStr = Move.serialize(move);
+        // integration test: check if Move outputted a serialized string using Piece
+        assertTrue(serializedStr.length() > 0 );
+
+    }
+}
