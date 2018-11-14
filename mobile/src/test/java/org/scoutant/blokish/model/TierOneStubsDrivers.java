573b40e95f4d00065811a4ed95e517408ab9cad7
PatrickGhazal <patrick.ghazal@mail.mcgill.ca>
2018-11-14 14:09:36 -0500

    Renamed stub classes to [ClassName]Stub

--- mobile/src/test/java/org/scoutant/blokish/model/TierOneStubsDrivers.java
+++ mobile/src/test/java/org/scoutant/blokish/model/TierOneStubsDrivers.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class TierOneStubsDrivers {
 	
-	Game game;
+	GameStub game;
 	static String tag = "sc";
 	
-	public TierOneStubsDrivers(Game g) {
+	public TierOneStubsDrivers(GameStub g) {
 		game = g;
 	}
 	
@@ -16,18 +16,18 @@ public class TierOneStubsDrivers {
 	 * Method (from the AI class) under test to verify the correct integration of AI and Game
 	 */
 	public boolean hasMove(int color) {
-		Board board = game.boards.get(color);
-		for (Square seed : board.seeds()) {
+		BoardStub board = game.boards.get(color);
+		for (SquareStub seed : board.seeds()) {
 			for (int p=0; p<board.pieces.size(); p++) {
-				Piece piece = board.pieces.get(p);
+				PieceStub piece = board.pieces.get(p);
 				// Fixing issue #3, changing order rotate/flip
 				for( int f=0; f<piece.flips; f++, piece.flip()) {
 					for (int r=0; r<piece.rotations; r++, piece.rotate(1)) { 
-						for (Square s : piece.squares()) {
+						for (SquareStub s : piece.squares()) {
 							int i = seed.i - s.i;
 							int j = seed.j - s.j;
 							if ( !board.outside(s, i, j) && game.fits(piece, i, j)) {
-								System.out.println(tag + "possible move : " + new Move(piece, i, j));
+								System.out.println(tag + "possible move : " + new MoveStub(piece, i, j));
 								game.boards.get(color).over = false;
 								return true;
 							}
@@ -44,23 +44,23 @@ public class TierOneStubsDrivers {
 /*
  * Original Game class with minor changes to improve testability (as detailed in the GitHub wiki)
  */
-class Game {
+class GameStub {
 	public static final String tag = "sc";
-	public List<Board> boards = new ArrayList<Board>();
+	public List<BoardStub> boards = new ArrayList<BoardStub>();
 	public int size = 20;
 	public int[] colors = { 0, 1, 2, 3 };
-	public Game() {
+	public GameStub() {
 		reset();
 	}
 	public void reset() {
 		boards.clear();
 		for(int k=0; k<4; k++) {
-			boards.add(new Board(k));
+			boards.add(new BoardStub(k));
 		}
 	}
 	
-	public List<Move> moves = new ArrayList<Move>();
-	public void historize(Move move) {
+	public List<MoveStub> moves = new ArrayList<MoveStub>();
+	public void historize(MoveStub move) {
 		moves.add(move);
 	}
 	
@@ -84,11 +84,11 @@ class Game {
 	}
 	
 	// to be called onto a fresh Game...
-	public boolean replay(List<Move> moves) {
-		for (Move move : moves) {
-			Piece piece = move.piece;
+	public boolean replay(List<MoveStub> moves) {
+		for (MoveStub move : moves) {
+			PieceStub piece = move.piece;
 			int color = piece.color; 
-			Piece p = boards.get(color).findPieceByType( piece.type);
+			PieceStub p = boards.get(color).findPieceByType( piece.type);
 			p.reset(piece);
 			move.piece = p;
 			boolean status = play(move);
@@ -97,23 +97,23 @@ class Game {
 		return true;
 	}
 	
-	protected void add( Piece piece, int i, int j) {
+	protected void add( PieceStub piece, int i, int j) {
 		for(int k=0; k<4; k++) {
 			boards.get(k).add(piece, i, j);
 		}
 	}
-	public boolean valid( Move move) {
+	public boolean valid( MoveStub move) {
 		return valid( move.piece, move.i, move.j);
 	}
-	public boolean valid( Piece piece, int i, int j) {
+	public boolean valid( PieceStub piece, int i, int j) {
 		return fits(piece, i, j)&& boards.get(piece.color).onseed(piece, i, j);
 	}
 	
-	public boolean fits( Piece p, int i, int j) {
+	public boolean fits( PieceStub p, int i, int j) {
 		return boards.get(0).fits(0,p, i, j) && boards.get(1).fits(1,p, i, j) && boards.get(2).fits(2,p, i, j) && boards.get(3).fits(3,p, i, j);
 	}
 	
-	public boolean play(Move move) {
+	public boolean play(MoveStub move) {
 		if ( ! valid(move)) {
 			System.out.println(tag + "not valid! " + move);
 			System.out.println(tag + "not valid! " + move.piece);
@@ -127,14 +127,14 @@ class Game {
 	
 	public String toString() {
 		String msg = "# moves : " + moves.size();
-		for (Move move: moves) {
-			msg += "\n" + Move.serialize(move);
+		for (MoveStub move: moves) {
+			msg += "\n" + MoveStub.serialize(move);
 		}
 		return msg;
 	}
 
-	public List<Move> deserialize(String msg) {
-		List<Move> list = new ArrayList<Move>();
+	public List<MoveStub> deserialize(String msg) {
+		List<MoveStub> list = new ArrayList<MoveStub>();
 		return list;
 	}
 	
@@ -143,14 +143,14 @@ class Game {
 	/**
 	 * @return # of seeds if actually adding enemy @param piece at @param i, @param j on board @param board.
 	 */
-	private int scoreEnemySeedsIfAdding(Board board, Piece piece, int i, int j) {
+	private int scoreEnemySeedsIfAdding(BoardStub board, PieceStub piece, int i, int j) {
 		// how many of the board's seeds happen to be under piece?
 		int result=0;
 		for (int b=0; b<20; b++) for (int a=0; a<20; a++) ab[a][b] = 0;
-		for(Square square : board.seeds()) {
+		for(SquareStub square : board.seeds()) {
 			try { ab[square.i][square.j] = 1; } catch (Exception e) {}
 		}
-		for(Square square : piece.squares()) {
+		for(SquareStub square : piece.squares()) {
 			try { ab[i+square.i][j+square.j] = 0; } catch (Exception e) {}
 		}
 		for (int b=0; b<20; b++) for (int a=0; a<20; a++) if (ab[a][b]==1) result++;
@@ -158,7 +158,7 @@ class Game {
 		return result;
 	}
 	
-	public int scoreEnemySeedsIfAdding(int color, Piece piece, int i, int j) {
+	public int scoreEnemySeedsIfAdding(int color, PieceStub piece, int i, int j) {
 		int result =0;
 //		for (int c=0; c<4; c++) {
 //			if (c!=color) {
@@ -175,41 +175,41 @@ class Game {
 /*
  * Stub for the Board class that contains the attributes and methods used in Game and/or AI.
  */
-class Board {
-	public List<Piece> pieces = new ArrayList<Piece>();
+class BoardStub {
+	public List<PieceStub> pieces = new ArrayList<PieceStub>();
 	public boolean over;
 	public int k;
 	public int score;
 	
-	public Board (int K) {
+	public BoardStub (int K) {
 		k = K;
 		over = false;
 		score = 0;
 	}
 	
-	public List<Square> seeds() {
-		return (new Piece()).squares();
+	public List<SquareStub> seeds() {
+		return (new PieceStub()).squares();
 	}
 	
-	public boolean outside(Square sq, int i, int j) {
+	public boolean outside(SquareStub sq, int i, int j) {
 		return false;
 	}
 	
 	public Piece findPieceByType(String t) {
 		if (pieces.size() > 0)
 			return pieces.get(0);
-		return new Piece();
+		return new PieceStub();
 	}
 	
-	public boolean fits(int a, Piece p, int b, int c) {
+	public boolean fits(int a, PieceStub p, int b, int c) {
 		return true;
 	}
 	
-	public void add(Piece p, int i, int j) {
+	public void add(PieceStub p, int i, int j) {
 		pieces.add(p);
 	}
 	
-	public boolean onseed(Piece p, int i, int j) {
+	public boolean onseed(PieceStub p, int i, int j) {
 		return true;
 	}
 }
@@ -217,29 +217,29 @@ class Board {
 /*
  * Stub for the Piece class that contains the attributes and methods used in Game and/or AI.
  */
-class Piece {
+class PieceStub {
 	public int flips;
 	public int rotations;
 	public int color;
 	public String type;
 	
-	public Piece flip() {
+	public PieceStub flip() {
 		return this;
 	}
 	
-	public Piece rotate(int i) {
+	public PieceStub rotate(int i) {
 		return this;
 	}
 	
-	public List<Square> squares() {
-		Square[] list = {new Square(1, 2), new Square(2, 3), new Square(3, 4)};
-		List<Square> arrList = new ArrayList<Square>();
-		for (Square sq : list)
+	public List<SquareStub> squares() {
+		SquareStub[] list = {new SquareStub(1, 2), new SquareStub(2, 3), new SquareStub(3, 4)};
+		List<SquareStub> arrList = new ArrayList<SquareStub>();
+		for (SquareStub sq : list)
 			arrList.add(sq);
 		return arrList;
 	}
 	
-	public void reset(Piece p) {
+	public void reset(PieceStub p) {
 		
 	}
 }
@@ -247,11 +247,11 @@ class Piece {
 /*
  * Stub for the Square class that contains the attributes and methods used in Game and/or AI.
  */
-class Square {
+class SquareStub {
 	public int i;
 	public int j;
 	
-	public Square(int I, int J) {
+	public SquareStub(int I, int J) {
 		i = I;
 		j = J;
 	}
@@ -260,12 +260,12 @@ class Square {
 /*
  * Stub for the Move class that contains the attributes and methods used in Game and/or AI.
  */
-class Move {
-	public Piece piece;
+class MoveStub {
+	public PieceStub piece;
 	public int i;
 	public int j;
 	
-	public Move(Piece p, int I, int J) {
+	public MoveStub(PieceStub p, int I, int J) {
 		piece = p;
 		i = I;
 		j = J;
