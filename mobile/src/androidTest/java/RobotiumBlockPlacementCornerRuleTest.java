b6397048c759cba08d70d384f2bea89a8728568b
Bao NGUYEN <bnguye31@ds2.CS.McGill.CA>
2018-12-02 15:33:29 -0500

    create barebones test file

---
+++ mobile/src/androidTest/java/RobotiumBlockPlacementCornerRuleTest.java
@@ -0,0 +1,8 @@
+package org.scoutant.blokish;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+public class RobotiumBlockPlacementCornerRuleTest extends ActivityInstrumentationTestCase2<SplashScreen>{
+    
+}
