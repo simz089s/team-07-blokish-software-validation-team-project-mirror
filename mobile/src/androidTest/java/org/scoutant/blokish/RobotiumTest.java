51398c6455fcff3f13dbfbadfeea486a84c29804
Elvric <elvric.trombert@mail.mcgill.ca>
2018-12-02 23:03:31 -0500

    Repair commit with the add and cancel feature tested

--- mobile/src/androidTest/java/org/scoutant/blokish/RobotiumTest.java
+++ mobile/src/androidTest/java/org/scoutant/blokish/RobotiumTest.java
@@ -3,10 +3,11 @@ package org.scoutant.blokish;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.robotium.solo.Solo;
-import org.scoutant.blokish.UI;
 
 public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
+
     private Solo solo;
+    private UI myUi;
 
     private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "org.scoutant.blokish.UI";
 
@@ -25,12 +26,12 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
         super((Class<UI>) launcherActivityClass);
     }
 
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
         solo = new Solo(getInstrumentation());
-
-        getActivity();
+        myUi = getActivity();
     }
 
     @Override
@@ -39,9 +40,30 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
         super.tearDown();
     }
 
-    public void testRegisterSucceeds() throws InterruptedException {
+    public void testCancelBlockPlacement() throws InterruptedException {
         solo.waitForActivity("UI", 2000);
+        newGame();
+        solo.drag(148, 37, 1346, 300, 20);
+        PieceUI myPiece = myUi.game.findPiece(0,"I5");
+        int x = myPiece.i0;
+        int y = myPiece.j0;
+        solo.clickLongOnScreen(296,1422, 1);
+        assertTrue(myPiece.i == x && myPiece.j == y);
+    }
 
-        assertFalse(false);
+    public void testAcceptBlockPlacement() throws InterruptedException {
+        solo.waitForActivity("UI", 2000);
+        newGame();
+        solo.drag(141, 11, 1340, 330, 20);
+        solo.clickLongOnScreen(730,1443);
+        assertTrue(solo.searchText("5"));
+    }
+
+    private void newGame(){
+        solo.drag(0f, 700f, 1346f, 1346f, 15);
+        solo.sleep(1000);
+        solo.clickLongOnScreen(300f, 800f);
+        solo.clickLongOnScreen(660f, 1100f);
     }
 }
+
