6f49950484ba7991ff24994b252c41c68f6f62c1
jz360 <jacqueshzhang97@gmail.com>
2018-12-02 23:12:00 -0500

    pour patrick

--- mobile/src/androidTest/java/org/scoutant/blokish/RobotiumTest.java
+++ mobile/src/androidTest/java/org/scoutant/blokish/RobotiumTest.java
@@ -40,6 +40,13 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
         super.tearDown();
     }
 
+    private void newGame(){
+        solo.drag(0f, 700f, 1346f, 1346f, 15);
+        solo.sleep(1000);
+        solo.clickLongOnScreen(300f, 800f);
+        solo.clickLongOnScreen(660f, 1100f);
+    }
+
     public void testCancelBlockPlacement() throws InterruptedException {
         solo.waitForActivity("UI", 2000);
         newGame();
@@ -59,11 +66,64 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
         assertTrue(solo.searchText("5"));
     }
 
-    private void newGame(){
-        solo.drag(0f, 700f, 1346f, 1346f, 15);
-        solo.sleep(1000);
-        solo.clickLongOnScreen(300f, 800f);
-        solo.clickLongOnScreen(660f, 1100f);
+    // scenario 3
+    public void testUpdateScoreEndGameNoBonus() throws InterruptedException {
+        solo.waitForActivity("UI", 2000);
+        //newGame();
+
+        UI myUi = getActivity();
+        int i = 0;
+        while (!myUi.game.game.over()) {
+            for (int j = 0; j < 4; j++) {
+                if (!myUi.game.game.over()) {
+                    myUi.think(j);
+                } else {
+                    break;
+                }
+
+            }
+        }
+        solo.sleep(5000);
+        assertTrue(true);
+//        solo.sleep(1000);
+//        solo.clickLongOnScreen(500f, 1100f);
+//        solo.sleep(800);
+//        solo.clickLongOnScreen(490f, 1200f);
+
     }
+
+
+
+    // scenario 6
+    /*public void testUpdateScoreDuringGame() throws InterruptedException {
+        solo.waitForActivity("UI", 2000);
+        newGame();
+
+        solo.drag(148f, 37f, 1346f, 300f, 15);
+        solo.clickLongOnScreen(750f, 1475f);
+        assertTrue(solo.searchText("5",4));
+    }*/
+
+    // scenario 7
+    /*public void testDragBlockCornerRuleFirstMove() throws InterruptedException {
+        solo.waitForActivity("UI", 2000);
+        newGame();
+        solo.drag(148f, 37f, 1346f, 320f, 15);
+        solo.clickLongOnScreen(750f, 1475f);
+
+        assertTrue(solo.searchText("5",4));
+    }
+
+    // scenario 8
+    public void testDragBlockCornerRuleFirstMoveFail() throws InterruptedException {
+        solo.waitForActivity("UI", 2000);
+        newGame();
+
+        solo.drag(148f, 60f, 1346f, 300f, 15);
+        solo.clickLongOnScreen(750f, 1475f);
+        boolean isFirstMoveValid = solo.waitForText("5", 1, 500);
+
+        assertFalse(isFirstMoveValid);
+    }*/
 }
 
