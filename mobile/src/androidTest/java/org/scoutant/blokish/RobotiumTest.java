5424b392157f448b3c59848ecd8dc5f1cf4b9bc7
Elvric <elvric.trombert@mail.mcgill.ca>
2018-12-03 15:06:08 -0500

    Almost final push

--- mobile/src/androidTest/java/org/scoutant/blokish/RobotiumTest.java
+++ mobile/src/androidTest/java/org/scoutant/blokish/RobotiumTest.java
@@ -5,6 +5,10 @@ import android.widget.ImageButton;
 
 import com.robotium.solo.Solo;
 
+import org.scoutant.blokish.model.Board;
+import org.scoutant.blokish.model.Game;
+import org.scoutant.blokish.model.Move;
+
 import static org.junit.Assert.assertNotEquals;
 
 public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
@@ -61,23 +65,25 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
 //        assertTrue(solo.searchText("0", 4));
 //    }
 //
-    public void testCancelBlockPlacement() throws InterruptedException {
-        solo.waitForActivity("UI", 2000);
-        newGame();
-        solo.drag(148, 37, 1346, 300, 20);
-        PieceUI myPiece = myUi.game.findPiece(0,"I5");
-        ImageButton okCancel =  myUi.game.buttons.getCancelButton();
-        solo.clickOnView(okCancel);
-        solo.sleep(100);
-        assertEquals(myPiece.i0, myPiece.i );
-        assertEquals(myPiece.j0, myPiece.j);
-    }
+//    public void testCancelBlockPlacement() throws InterruptedException {
+//        solo.waitForActivity("UI", 2000);
+//        newGame();
+//        solo.drag(148, 37, 1346, 300, 20);
+//        PieceUI myPiece = myUi.game.findPiece(0,"I5");
+//        ImageButton cancelButton =  myUi.game.buttons.getCancelButton();
+//        solo.clickOnView(okCancel);
+//        solo.sleep(100);
+//        assertEquals(myPiece.i0, myPiece.i );
+//        assertEquals(myPiece.j0, myPiece.j);
+//    }
 //
 //    public void testAcceptBlockPlacement() throws InterruptedException {
 //        solo.waitForActivity("UI", 2000);
 //        newGame();
 //        solo.drag(141, 11, 1340, 330, 25);
-//        solo.clickLongOnScreen(730,1443);
+//        ImageButton okButton =  myUi.game.buttons.ok;
+//        solo.clickOnView(okButton);
+//        solo.sleep(100);
 //        PieceUI myPiece = myUi.game.findPiece(0,"I5");
 //        assertEquals(0, myPiece.i);
 //        assertEquals(2, myPiece.j);
@@ -86,16 +92,16 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
          // scenario 4
     public void testUpdateScoreEndGameBonusSingleSquare() throws InterruptedException {
         solo.waitForActivity("UI", 2000);
-        //newGame();
+        newGame();
 
         UI myUi = getActivity();
-        int i = 0;
         int currentScore = 0;
-        Board userBoard;
+        Board userBoard = null;
         while (!myUi.game.game.over()) {
             for (int j = 0; j < 4; j++) {
                 if (!myUi.game.game.over()) {
                     myUi.think(j);
+                    solo.sleep(500);
                 } else {
                 	Game currentGame = myUi.game.game;
                     userBoard = currentGame.boards.get(0);
@@ -114,52 +120,42 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
         }
         solo.sleep(5000);
         assertEquals(userBoard.score, currentScore + 5);
-//        solo.sleep(1000);
-//        solo.clickLongOnScreen(500f, 1100f);
-//        solo.sleep(800);
-//        solo.clickLongOnScreen(490f, 1200f);
-
     }
     
     
     // scenario 5
-    public void testUpdateScoreEndGameBonusAllSquares() throws InterruptedException {
-        solo.waitForActivity("UI", 2000);
-        //newGame();
-
-        UI myUi = getActivity();
-        int i = 0;
-        int currentScore = 0;
-        Board userBoard;
-        while (!myUi.game.game.over()) {
-            for (int j = 0; j < 4; j++) {
-                if (!myUi.game.game.over()) {
-                    myUi.think(j);
-                } else {
-                	Game currentGame = myUi.game.game;
-                    userBoard = currentGame.boards.get(0);
-                    currentScore = userBoard.score;
-                    int pieceCounter = 0;
-                    for (Move move : currentGame.moves) {
-                    	if (move.piece.color == userBoard.color) {
-                    		pieceCounter++;
-                    	}
-                    }
-                    if (pieceCounter == 21) {
-                    	userBoard.score += 15;
-                    }
-                }
-
-            }
-        }
-        solo.sleep(5000);
-        assertEquals(userBoard.score, currentScore + 15);
-//        solo.sleep(1000);
-//        solo.clickLongOnScreen(500f, 1100f);
-//        solo.sleep(800);
-//        solo.clickLongOnScreen(490f, 1200f);
-
-}
+//    public void testUpdateScoreEndGameBonusAllSquares() throws InterruptedException {
+//        solo.waitForActivity("UI", 2000);
+//        newGame();
+//        UI myUi = getActivity();
+//        int currentScore = 0;
+//        Board userBoard = null;
+//        while (!myUi.game.game.over()) {
+//            for (int j = 0; j < 4; j++) {
+//                if (!myUi.game.game.over()) {
+//                    myUi.think(j);
+//                    solo.sleep(500);
+//                } else {
+//                	Game currentGame = myUi.game.game;
+//                    userBoard = currentGame.boards.get(0);
+//                    currentScore = userBoard.score;
+//                    int pieceCounter = 0;
+//                    for (Move move : currentGame.moves) {
+//                    	if (move.piece.color == userBoard.color) {
+//                    		pieceCounter++;
+//                    	}
+//                    }
+//                    if (pieceCounter == 21) {
+//                    	userBoard.score += 15;
+//                    }
+//                }
+//
+//            }
+//        }
+//        solo.sleep(5000);
+//        assertEquals(userBoard.score, currentScore + 15);
+//
+//}
     
     
 //
@@ -167,12 +163,12 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
 //    public void testUpdateScoreDuringGame() throws InterruptedException {
 //        solo.waitForActivity("UI", 2000);
 //        newGame();
-//
+//        ImageButton okButton =  myUi.game.buttons.ok;
 //        solo.drag(148f, 37f, 1346f, 300f, 25);
-//        solo.clickLongOnScreen(750f, 1475f);
+//        solo.clickOnView(okButton);
 //        solo.drag(195f, 58f, 1576f, 570f, 25);
-//        solo.clickLongOnScreen(750f, 1475f);
-//
+//        solo.clickOnView(okButton);
+//        solo.sleep(100);
 //        assertEquals("10", myUi.game.tabs[0].getText().toString());
 //    }
 
@@ -183,6 +179,7 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
 //
 //        solo.drag(141, 11, 1340, 330, 25); // Drag piece to corner
 //        solo.sleep(100);
+//        ImageButton okButton =  myUi.game.buttons.ok;
 //        boolean okButtonIsEnabled = okButton.isEnabled();
 //        assertTrue(okButtonIsEnabled); // OK button should be clickable
 //    }
@@ -193,7 +190,7 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
 //        newGame(); // Restart game
 //
 //        solo.drag(148, 100, 1346, 300, 20); // Drag piece to center
-//
+//        ImageButton okButton =  myUi.game.buttons.ok;
 //        boolean okButtonIsEnabled = okButton.isEnabled();
 //        assertFalse(okButtonIsEnabled); // OK button should not be clickable
 //    }
@@ -229,7 +226,7 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
 //        solo.drag(141, 11, 1340, 330, 25);
 //        ImageButton okButton =  myUi.game.buttons.ok;
 //        solo.clickOnView(okButton);
-//        solo.drag(195f, 58f, 1576f, 570f, 25);
+//        solo.drag(195, 58, 1576, 570, 25);
 //        boolean okButtonIsEnabled = okButton.isEnabled();
 //        assertTrue(okButtonIsEnabled);
 //    }
@@ -241,7 +238,7 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
 //        solo.drag(141, 11, 1340, 330, 25);
 //        ImageButton okButton =  myUi.game.buttons.ok;
 //        solo.clickOnView(okButton);
-//        solo.drag(195f, 258f, 1576f, 370f, 25);
+//        solo.drag(195, 258, 1576, 400, 25);
 //        boolean okButtonIsEnabled = okButton.isEnabled();
 //        assertFalse(okButtonIsEnabled);
 //    }
@@ -253,7 +250,21 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<UI> {
 //        solo.drag(141, 11, 1340, 330, 25);
 //        ImageButton okButton =  myUi.game.buttons.ok;
 //        solo.clickOnView(okButton);
-//        solo.drag(195f, 358f, 1576f, 570f, 25);
+//        solo.drag(195, 500, 1576, 570, 25);
+//        boolean okButtonIsEnabled = okButton.isEnabled();
+//        assertFalse(okButtonIsEnabled);
+//    }
+
+    //    // scenario 13
+//    public void testBlockPlacementCornerRuleViolatedCornerNotTouching()throws InterruptedException{
+//        solo.waitForActivity("UI", 2000);
+//        newGame();
+//        solo.drag(141, 11, 1340, 330, 25);
+//        ImageButton okButton =  myUi.game.buttons.ok;
+//        solo.clickOnView(okButton);
+//        solo.sleep(100);
+//        solo.drag(468, 114, 1345, 370, 25);
+//        solo.sleep(100);
 //        boolean okButtonIsEnabled = okButton.isEnabled();
 //        assertFalse(okButtonIsEnabled);
 //    }
